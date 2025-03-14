6da406b4feb0c592336f97334140458772df1923
Daniel C. Silva <danielcs80@gmail.com>
2025-03-14 00:30:24 -0300

    Calculadora Aritimética e Lógica

--- aula02/aula02.js
+++ aula02/aula02.js
@@ -1,11 +1,15 @@
-let select = document.getElementById('operador');
+let opAritmetico = document.getElementById('opAritmetico');
+let opLogico = document.getElementById('opLogico');
+let x1 = document.getElementById('x1');
+let x2 = document.getElementById('x2'); 
+let l1 = document.getElementById('l1');
+let l2 = document.getElementById('l2');
+let resultado = document.getElementById('resultado');
 
 
-select.addEventListener('change',function (){
-    let operacao = document.getElementById('operador').value;
+
+function calculadoraAritmetica(operacao, x1, x2) {
     let resultado = 0;
-    let x1 = parseFloat(document.getElementById('x1').value);
-    let x2 = parseFloat(document.getElementById('x2').value);
     if (operacao === '+') {
         resultado = x1 + x2;
     } else if (operacao === '-') {
@@ -16,4 +20,29 @@ select.addEventListener('change',function (){
         resultado = x1 / x2;
     }
     document.getElementById('resultado').innerHTML = resultado;
-})
+}
+
+function calculadoraLogica(operacao, x1, x2) {
+    let resultado = 0;
+    if (operacao === '&&') {
+        resultado = x1 && x2;
+    } else if (operacao === '||') {
+        resultado = x1 || x2;
+    } else if (operacao === '!') {
+        resultado = !x1;
+    }
+    document.getElementById('resultadoLogico').innerHTML = resultado;
+}
+
+x1.addEventListener('input', function() {
+    calculadoraAritmetica(opAritmetico.value, parseFloat(x1.value), parseFloat(x2.value));
+});
+x2.addEventListener('input', function() {
+    calculadoraAritmetica(opAritmetico.value, parseFloat(x1.value), parseFloat(x2.value));
+});
+opAritmetico.addEventListener('input', function() {
+    calculadoraAritmetica(opAritmetico.value, parseFloat(x1.value), parseFloat(x2.value));
+});
+opLogico.addEventListener('input', function() {
+    calculadoraLogica(opLogico.value, l1.checked, l2.checked);
+});
